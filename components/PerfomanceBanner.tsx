7852372589c3b34d3ca1ebfbc5f87cc2a81c8ecd
Andrei <andreiwork25@gmail.com>
2024-12-10 05:39:03 +0800

    Update PerfomanceBanner.tsx

--- components/PerfomanceBanner.tsx
+++ components/PerfomanceBanner.tsx
@@ -2,22 +2,26 @@ import { BannerContent } from "@/constants";
 import Image from "next/image";
 
 export default function PerformanceBanner() {
-    return(
-        <main className="w-full lg:h-[350px] bg-slate-100 bg-cover bg-bottom py-20 lg:py-0">
-            <div className="w-full h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-0 place-content-center">
-            {BannerContent.map((block) => (
-                <div key={block.id} className="h-[180px] lg:h-[220px] lg:w-[250px] aspect-video bg-white rounded-xl p-8 flex flex-row flex-wrap justify-center items-center text-center shadow-2xl hover:scale-105 linear duration-100 transition place-self-center">
-                    <Image 
-                        src={block.icon}
-                        height={150}
-                        width={100}
-                        alt={block.alt}
-                        >
-                    </Image>
-                    <h1 className="w-full font-semibold text-lg capitalize">{block.title}</h1>
-                </div>
-            ))}
-            </div>
-        </main>
-    )
+  return (
+    <main className="w-full bg-slate-100 bg-cover bg-bottom py-20 lg:h-[350px] lg:py-0">
+      <div className="grid h-full w-full grid-cols-1 place-content-center gap-8 md:grid-cols-2 lg:grid-cols-4 lg:gap-0">
+        {BannerContent.map((block) => (
+          <div
+            key={block.id}
+            className="linear flex aspect-video h-[180px] flex-row flex-wrap items-center justify-center place-self-center rounded-xl bg-white p-8 text-center shadow-2xl transition duration-100 hover:scale-105 lg:h-[220px] lg:w-[250px]"
+          >
+            <Image
+              src={block.icon}
+              height={150}
+              width={100}
+              alt={block.alt}
+            ></Image>
+            <h1 className="w-full text-lg font-semibold capitalize">
+              {block.title}
+            </h1>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
