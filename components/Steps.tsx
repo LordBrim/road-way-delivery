5366e5defe28cbeb68814d342f68c3ff35dbae43
Andrei <andreiwork25@gmail.com>
2024-12-10 06:46:31 +0800

    Added steps mockups

--- components/Steps.tsx
+++ components/Steps.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Steps() {
   return (
     <div className="flex w-full flex-col items-center justify-center">
@@ -8,13 +10,14 @@ export default function Steps() {
           <span className="text-accent-1">Step-by-Step</span>
         </h1>
         <div className="flex w-full flex-col justify-center gap-8 md:flex-row md:gap-12">
-          {StepsData.map(({ number, title, desc, image }, id) => (
+          {StepsData.map(({ number, title, desc, src, alt }, index) => (
             <Step
-              key={id}
+              key={index}
               number={number}
               title={title}
               desc={desc}
-              image={image}
+              src={src}
+              alt={alt}
             />
           ))}
         </div>
@@ -27,13 +30,17 @@ type StepProps = {
   number: number;
   title: string;
   desc: string;
-  image: string;
+  src: string;
+  alt: string;
 };
 
-function Step({ number, title, desc, image }: StepProps) {
+function Step({ number, title, desc, src, alt }: StepProps) {
   return (
     <div className="flex w-full max-w-[420px] flex-col gap-4 md:gap-16 md:[&:nth-of-type(2)]:mt-[100px] md:[&:nth-of-type(3)]:mt-[200px]">
-      <div className="relative aspect-video w-full max-w-[420px] rounded-2xl bg-gray-500">
+      <div className="relative w-full max-w-[420px] rounded-2xl bg-gray-500">
+        <div className="h-full w-full overflow-hidden shadow-lg">
+          <Image src={src} alt={alt} width={1000} height={1000} />
+        </div>
         <div className="-bottom-10 -left-10 hidden h-20 w-20 items-center justify-center rounded-t-full rounded-bl-full bg-accent-1 md:absolute md:flex">
           <p className="text-4xl font-bold text-white">{number}</p>
         </div>
@@ -55,18 +62,21 @@ const StepsData = [
     number: 1,
     title: "Set Pickup & Destination",
     desc: "Just enter your pickup and delivery locations, and we’ll find the fastest, most cost-effective route for your items.",
-    image: "",
+    src: "/rwd-step-1.jpg",
+    alt: "Step 1",
   },
   {
     number: 2,
     title: "Track in Real Time",
     desc: "Stay updated with real-time tracking, from pickup to drop-off. Never lose sight of your delivery again.",
-    image: "",
+    src: "/rwd-step-2.jpg",
+    alt: "Step 2",
   },
   {
     number: 3,
     title: "Confirm Delivery",
     desc: "Once delivered, you'll receive instant confirmation and proof of delivery right in the app.",
-    image: "",
+    src: "/rwd-step-3.jpg",
+    alt: "Step 3",
   },
 ];
