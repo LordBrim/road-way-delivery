41bc82164ef2168d4565ba9bf2a00743348e279a
AndreiSager <andraxiusabyss@proton.me>
2025-01-22 15:47:45 +0800

    style(newsletterbanner): align input and button into one row

--- components/NewsletterBanner.tsx
+++ components/NewsletterBanner.tsx
@@ -30,18 +30,18 @@ export default function NewsletterBanner() {
                 latest news from Road Way Delivery.
               </p>
             </div>
-            <form className="z-10 flex w-full flex-col items-center gap-2 lg:flex-row">
+            <form className="z-10 flex w-full flex-row items-center gap-2 p-4">
               <input
                 type="email"
                 name="email"
                 placeholder="Enter your email..."
                 value={email}
-                className="w-full max-w-[480px] rounded-full px-6 py-2 text-base font-medium focus:placeholder:italic"
+                className="w-2/3 max-w-[480px] rounded-full px-6 py-2 text-base font-medium focus:placeholder:italic md:w-full"
                 onChange={(e) => setEmail(e.target.value)}
               />
               <button
                 type="button"
-                className="rounded-full bg-accent-1 px-6 py-2 text-center text-base font-medium text-white"
+                className="text-nowrap w-1/3 rounded-full bg-accent-1 px-6 py-2 text-center text-base font-medium text-white md:w-auto"
               >
                 Join Now
               </button>
