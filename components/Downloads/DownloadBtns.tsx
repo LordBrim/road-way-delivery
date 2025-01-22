4efe20937a60126c488c47eab58ed8dda2ce6fd7
AndreiSager <andraxiusabyss@proton.me>
2025-01-22 15:37:19 +0800

    style(downloadbtns): fix text wrap

--- components/Downloads/DownloadBtns.tsx
+++ components/Downloads/DownloadBtns.tsx
@@ -12,7 +12,7 @@ export default function DownloadBtns() {
     <div className="flex flex-row gap-6">
       <button
         onClick={() => handleButton()}
-        className="group flex select-none flex-row items-center justify-center gap-8 rounded-full bg-black py-2 pl-8 pr-2 duration-300 ease-out hover:-translate-y-1 hover:bg-[#1a1a1a] hover:shadow-xl hover:brightness-125"
+        className="group flex select-none flex-row items-center justify-center gap-4 rounded-full bg-black py-2 pl-8 pr-2 duration-300 ease-out hover:-translate-y-1 hover:bg-[#1a1a1a] hover:shadow-xl hover:brightness-125"
       >
         <p className="text-white">Play Store</p>
         <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#3D3D3D] group-hover:bg-[#4a4a4a]">
@@ -26,7 +26,7 @@ export default function DownloadBtns() {
       </button>
       <button
         onClick={() => handleButton()}
-        className="group flex select-none flex-row items-center justify-center gap-8 rounded-full bg-black py-2 pl-8 pr-2 duration-300 ease-out hover:-translate-y-1 hover:bg-[#1a1a1a] hover:shadow-xl hover:brightness-125"
+        className="group flex select-none flex-row items-center justify-center gap-4 rounded-full bg-black py-2 pl-8 pr-2 duration-300 ease-out hover:-translate-y-1 hover:bg-[#1a1a1a] hover:shadow-xl hover:brightness-125"
       >
         <p className="text-white">App Store</p>
         <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#3D3D3D] group-hover:bg-[#4a4a4a]">
